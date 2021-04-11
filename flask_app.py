26224bdc5580a0b788babc9f96f714467aa6e8f9
Peter Amerkhanian <peteamer@gmail.com>
2021-04-11 12:09:08 -0700

    initial commit

---
+++ flask_app.py
@@ -0,0 +1,22 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index2.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/order')
+def order():
+    return render_template("order.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
