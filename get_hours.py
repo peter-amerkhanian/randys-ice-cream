05655bcf4319e40427dab288a4b4fcd1f9b7ac93
Peter <peteamer@gmail>
2021-06-18 22:23:38 +0000

    server changes no good

--- get_hours.py
+++ get_hours.py
@@ -3,7 +3,7 @@ import csv
 schedule_raw = []
 
 # Open .csv and reduce to list of lists
-with open("static/schedule.csv", mode="r") as f:
+with open("/home/nbrandon62/randys-ice-cream/static/schedule.csv", mode="r") as f:
     schedule_csv = csv.reader(f)
     fields = next(schedule_csv)
     for row in schedule_csv:
